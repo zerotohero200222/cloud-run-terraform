201c5625d601852a541f1630e91a689c9ade448d
zerotohero200222 <aebharath17@gmail.com>
2025-09-16 17:10:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
+output "cloud_run_service_name" {
+  description = "The name of the deployed Cloud Run service"
+  value       = google_cloud_run_service.default.name
+}
+
+output "cloud_run_region" {
+  description = "Region where the Cloud Run service is deployed"
+  value       = google_cloud_run_service.default.location
+}
+
 output "cloud_run_url" {
-  description = "The URL of the deployed Cloud Run service"
+  description = "The public URL of the Cloud Run service"
   value       = google_cloud_run_service.default.status[0].url
 }
