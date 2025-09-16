42b35312b0413aa96b712e8878d974f3743599c8
zerotohero200222 <aebharath17@gmail.com>
2025-09-16 17:17:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,9 @@ resource "google_cloud_run_service" "default" {
   autogenerate_revision_name = true
 }
 
-# Allow unauthenticated (public) access
 resource "google_cloud_run_service_iam_member" "invoker" {
   location = google_cloud_run_service.default.location
   service  = google_cloud_run_service.default.name
   role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-output "cloud_run_url" {
-  description = "Public URL of the Cloud Run service"
-  value       = google_cloud_run_service.default.status[0].url
+  member   = "allUsers" # make service public
 }
